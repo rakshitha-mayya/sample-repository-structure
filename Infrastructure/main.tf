e961de687563f0aa341304ea07a71e2a68f80af0
rakshitha-mayya <113757274+rakshitha-mayya@users.noreply.github.com>
2025-09-06 23:11:53 +0530

    Update main.tf

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -236,7 +236,7 @@ resource "azurerm_lb_rule" "alb_rule" {
 # ------------------------------------------
 
 provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
