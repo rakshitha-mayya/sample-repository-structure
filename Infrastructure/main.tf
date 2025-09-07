478bf2a9b8315bce19c70177f0191365de5945da
rakshitha-mayya <113757274+rakshitha-mayya@users.noreply.github.com>
2025-09-07 11:41:29 +0530

    Update main.tf

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -248,12 +248,13 @@ provider "kubernetes" {
 # Helm Provider (for ArgoCD)
 # ------------------------------------------
 provider "helm" {
+  alias      = "aks"  
   kubernetes {
     host                   = azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
-    alias      = "aks"  
+    
 }
 }
 
